ce01113f8ca8af2618c5318b11c797a4aa146816
AnvarHadja <74791981+AnvarHadja@users.noreply.github.com>
2024-10-15 20:33:51 -0700

    Adds constructer args and ownable tested

---
+++ test/NFT.js
@@ -0,0 +1,63 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+
+describe('NFT', () => {
+    let nft, deployer, minter
+    const NAME = 'AI Punks'
+    const SYMBOL = 'AIP'
+    const COST = ether(10)
+    const MAX_SUPPLY = 25
+    const BASE_URI = 'ipfs://QmQPEMsfd1tJnqYPbnTQCjoa8vczfsV1FmqZWgRdNQ7z3g/'
+
+    beforeEach(async () => {
+        
+        let accounts = await ethers.getSigners()
+        deployer = accounts[0]
+        minter = accounts[1]
+    })
+
+    describe('Deployment', () => {
+
+        const ALLOW_MINTING_ON = (Date.now() + 120000).toString().slice(0, 10) // 2 mins from now
+
+        beforeEach(async () => {
+            const NFT = await ethers.getContractFactory('NFT')
+            nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
+        })
+
+        it('has correct name', async () => {
+            expect(await nft.name()).to.eq(NAME)
+        })
+        
+        it('has correct symbol', async () => {
+            expect(await nft.symbol()).to.eq(SYMBOL)
+        })
+
+        it('returns the cost to mint', async () => {
+            expect(await nft.cost()).to.eq(COST)
+        })
+
+        it('returns the max total supply', async () => {
+            expect(await nft.maxSupply()).to.eq(MAX_SUPPLY)
+        })
+
+        it('returns the allowed minting time', async () => {
+            expect(await nft.allowMintingOn()).to.eq(ALLOW_MINTING_ON)
+        })
+
+        it('returns the base URI', async () => {
+            expect(await nft.baseURI()).to.eq(BASE_URI)
+        })
+
+        it('returns the owner', async () => {
+            expect(await nft.owner()).to.eq(deployer.address)
+        })
+    })
+
+})
