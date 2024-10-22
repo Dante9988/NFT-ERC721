4759c1ce8a31c23a28de91a38e2701a31738970f
AnvarHadja <74791981+AnvarHadja@users.noreply.github.com>
2024-10-22 14:28:16 -0700

    Adds deploys NFT

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const DEFAULT_OWNER = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat:{
+      allowUnlimitedContractSize: true,
+    },   
+    localhost:{
+      url: "http://localhost:8545"
+    },
+    cc3: {
+      url: 'https://rpc.cc3-testnet.creditcoin.network',
+      chainId: 102031,
+      accounts: [`${DEFAULT_OWNER}`],
+      gas: 5000000,
+      gasPrice: 20000000000,
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      chainId: 11155111,
+      accounts: [`${DEFAULT_OWNER}`],
+    } 
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,17 +6,26 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+const tokens = (n) => {
+  return hre.ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+
 async function main() {
-  const NAME = 'Dapp University'
-  const SYMBOL = 'DAPP'
-  const MAX_SUPPLY = '1000000'
+  const NAME = 'AI Punks'
+  const SYMBOL = 'AIP'
+  const COST = ether(10)
+  const MAX_SUPPLY = 25
+  const BASE_URI = 'ipfs://QmQPEMsfd1tJnqYPbnTQCjoa8vczfsV1FmqZWgRdNQ7z3g/'
+  const ALLOW_MINTING_ON = (Date.now() + 120000).toString().slice(0, 10) // 2 mins from now
 
   // Deploy Token
-  const Token = await hre.ethers.getContractFactory('Token')
-  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
+  const NFT = await hre.ethers.getContractFactory('NFT')
+  let nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
 
-  await token.deployed()
-  console.log(`Token deployed to: ${token.address}\n`)
+  await nft.deployed()
+  console.log(`NFT deployed to: ${nft.address}\n`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
